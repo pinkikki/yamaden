4fe3246fb282604aa0e45bab8f9ff41fe443d468
pinkikki <w-mizutani@pinkikki.jp>
2018-07-02 22:27:27 +0900

    add yamaden

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private List<AudioSource> _bgmList;
+    private AudioSource _currentBgm;
+
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance != null) return _instance;
+            _instance = FindObjectOfType<AudioManager>();
+            if (_instance == null)
+            {
+                Debug.LogError(typeof(AudioManager) + " is none");
+            }
+            return _instance;
+        }
+    }
+
+    void Start()
+    {
+        _currentBgm = _bgmList[0];
+    }
+    
+    public void PlayBgm(int index)
+    {
+        _currentBgm.Stop();
+        _currentBgm = _bgmList[index];
+        _currentBgm.time = 0.0f;
+        _currentBgm.Play();
+    }
+
+    public void StopBgm()
+    {
+        _currentBgm.Stop();
+        _currentBgm.clip = null;
+    }
+}
---
+++ Assets/scripts/YamadenSystem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+public class YamadenSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject _yamaUniversityFirstGradeObj;
+    [SerializeField] private GameObject _yamaUniversityFourthGradeObj;
+    [SerializeField] private GameObject _yamaMister;
+
+    
+    public void FirstGradeClick()
+    {
+        SetActive(true, false, false);
+        AudioManager.Instance.PlayBgm(0);
+    }
+
+    public void FourthGradeClick()
+    {
+        SetActive(false, true, false);
+        AudioManager.Instance.PlayBgm(1);
+    }
+
+    public void MisterClick()
+    {
+        SetActive(false, false, true);
+        AudioManager.Instance.PlayBgm(2);
+    }
+
+    private void SetActive(bool firstGradeObjEnabled, bool fourthGradeObjEnabled, bool misterEnabled)
+    {
+        _yamaUniversityFirstGradeObj.SetActive(firstGradeObjEnabled);
+        _yamaUniversityFourthGradeObj.SetActive(fourthGradeObjEnabled);
+        _yamaMister.SetActive(misterEnabled);
+    }
+}
